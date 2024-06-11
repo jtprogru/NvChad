ceb15f89195d9487a3ff7192ba1fd9146a6396ec
Michael Savin <jtprogru@gmail.com>
2024-06-11 21:48:58 +0300

    Add key map for moving lines up and down

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,3 +9,9 @@ map("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "Run LazyGit", nowait = true
 map("v", ">", ">gv", { desc = "indent" })
 map("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Toggle TODO list" })
 map("n", "<leader>tt", "<cmd>lua require('base46').toggle_theme()<cr>", { desc = "Toggle Light/Dark Theme" })
+
+map("n", "<A-j>", ":m .+1<CR>==") -- move line up(n)
+map("n", "<A-k>", ":m .-2<CR>==") -- move line down(n)
+
+map("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
+map("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line down(v)
