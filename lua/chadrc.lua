02658728a8c15d11e0c10f526f388e677db7e031
Michael Savin <jtprogru@gmail.com>
2024-06-11 20:18:45 +0300

    Remove comments

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,5 +1,3 @@
--- This file  needs to have same structure as nvconfig.lua
--- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 local highlights = require "configs.highlights"
 
 local M = {}
